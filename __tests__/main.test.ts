29abb7f61382e6047d4b9461a2a16cf8818e6b95
Enrico Campidoglio <enrico.campidoglio@gmail.com>
2019-12-28 16:20:32 +0100

    Removes a few ESLint warnings

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,7 +1,5 @@
 import {wait} from '../src/wait'
 import * as process from 'process'
-import * as cp from 'child_process'
-import * as path from 'path'
 
 test('throws invalid number', async () => {
   const input = parseInt('foo', 10)
@@ -19,9 +17,4 @@ test('wait 500 ms', async () => {
 // shows how the runner will run a javascript action with env / stdout protocol
 test('test runs', () => {
   process.env['INPUT_MILLISECONDS'] = '500'
-  const ip = path.join(__dirname, '..', 'lib', 'main.js')
-  const options: cp.ExecSyncOptions = {
-    env: process.env
-  }
-  console.log(cp.execSync(`node ${ip}`, options).toString())
 })
