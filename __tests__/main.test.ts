2793fc7f47bf1a6770dc2cb201af7f276d7e42b8
Enrico Campidoglio <enrico.campidoglio@gmail.com>
2019-12-31 14:57:03 +0100

    Verifies that the created tag contains a SemVer version

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,10 +1,17 @@
 import * as core from '@actions/core'
+import * as event from '../src/event'
 import {run} from '../src/main'
 
 jest.mock('@actions/core')
+jest.mock('../src/event')
 
 describe('When running the action', () => {
+  const fakeGetCreatedTag = event.getCreatedTag as jest.MockedFunction<typeof event.getCreatedTag>
   const fakeSetOutput = core.setOutput as jest.MockedFunction<typeof core.setOutput>
+
+  beforeAll(() => {
+    fakeGetCreatedTag.mockReturnValue('v1.0.0')
+  })
 
   test('it should set the release-url output parameter', async () => {
     await run()
@@ -13,10 +20,12 @@ describe('When running the action', () => {
 })
 
 describe('When the action fails', () => {
+  const fakeGetCreatedTag = event.getCreatedTag as jest.MockedFunction<typeof event.getCreatedTag>
   const fakeSetOutput = core.setOutput as jest.MockedFunction<typeof core.setOutput>
   const fakeSetFailed = core.setFailed as jest.MockedFunction<typeof core.setFailed>
 
   beforeAll(() => {
+    fakeGetCreatedTag.mockReturnValue('v1.0.0')
     fakeSetOutput.mockImplementation(() => {
       throw new Error('Failed to set the output parameter')
     })
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,15 @@
 import * as core from '@actions/core'
+import * as event from './event'
+import * as version from './version'
 
 export async function run(): Promise<void> {
   try {
+    const tag = event.getCreatedTag()
+
+    if (tag && version.isSemVer(tag)) {
+      // TODO
+    }
+
     core.setOutput('release-url', 'https://example.com')
   } catch (error) {
     core.setFailed(error.message)
